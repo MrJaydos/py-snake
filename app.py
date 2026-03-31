2a4ad1094778045b185f35cabc221e8603c6b31f
Jayden Dickinson <jaydend93@gmail.com>
2026-03-31 16:10:27 +1100

    Importing libraries, setting window size, setting colors for the game, setting snake speed

---
+++ app.py
@@ -0,0 +1,14 @@
+# importing libraries
+import pygame
+import time
+import random
+
+# Snake Speed Varialble 
+snake_speed = 15
+
+#defining colours
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
