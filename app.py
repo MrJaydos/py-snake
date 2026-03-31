6ae6342b92cd26d9327ca12a3b2884f0f9db8492
Jayden Dickinson <jaydend93@gmail.com>
2026-03-31 16:18:48 +1100

    Initaialised pygame, set window to display and added FPS

--- app.py
+++ app.py
@@ -10,9 +10,20 @@ snake_speed = 15
 window_x = 720
 window_y = 480
 
-#defining colours
+# defining colours
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255,255,255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
+
+# Initialising pygame
+pygame.init()
+
+# initialise game window
+pygame.display.set_caption('Jaydos Snake')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# FPS controller
+fps = pygame.time.Clock()
+
