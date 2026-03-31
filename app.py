e02a351d25ffc21f96c9266a3f1c0c75d1d5a18c
Jayden Dickinson <jaydend93@gmail.com>
2026-03-31 16:30:59 +1100

    Added original snake position, fruit position, and the direction the snake goes in from teh start

--- app.py
+++ app.py
@@ -27,3 +27,23 @@ game_window = pygame.display.set_mode((window_x, window_y))
 # FPS controller
 fps = pygame.time.Clock()
 
+# Defining snake default position
+snake_position = [100, 50]
+
+# Defining first 4 blocks of snake
+# Body
+snake_body = [  [100, 50]
+                [90, 50]
+                [80, 50]
+                [70, 50]
+                ]
+
+# Fruit Position
+fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                  random.randrange(1, (window_y//10)) * 10]
+fruit_spawn = True
+
+# Setting default snake direction
+# Towards right
+direction = 'RIGHT'
+change_to = direction
